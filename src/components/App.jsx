12d54795db98856c431a57a65fb2c1f0b8c73544
reyesjerez <reyesjerezpe@gmail.com>
2024-02-03 09:25:52 +0100

    Add basic html and scss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,34 @@
-import '../scss/App.scss'
+import "../scss/App.scss";
+
+import dataFriends from "../data/dataFriends.json"
+
 
 function App() {
   return (
-    <div>
-      Escribe aquí tu código
+    <div className="container">
+      <header >
+        <h1 className="title">Frases de friends</h1>
+      </header>
+      <main>
+        <form className="form">
+          <label htmlFor="friends-text">Filtrar por frase:</label>
+          <input className="form__filter" type="text" id="friends-text" placeholder="Escribe aquí:"/>
+          <label htmlFor="friends-name">Filtrar por personaje:</label>
+          <input className="form__filter" type="select" id="friends-name">
+          </input>
+        </form>
+        <ul className="list">
+          <li className="list__item">
+            <p className="list__item__text">Pivot! Pivot! Pivot! Pivot! Pivot! - <span className="list__item__character">Ross</span></p>  
+          </li>
+          <li className="list__item">
+            <p className="list__item__text">Pivot! Pivot! Pivot! Pivot! Pivot! - <span className="list__item__character">Ross</span></p>  
+          </li>
+        </ul>
+
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
